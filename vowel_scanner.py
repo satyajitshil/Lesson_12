a245caac230c842d7b531d27b072b1df2f0d7c3a
Satyajit Shil <satyajitshil02@gmail.com>
2025-07-26 21:41:53 -0700

    vowel_scanner

---
+++ vowel_scanner.py
@@ -0,0 +1,21 @@
+
+def count_vowels():
+    name = input("Type a word:")
+
+    vowels = ['a','e','i','o','u']
+    count = 0
+    for c in name:
+        if c in vowels:
+            count += 1
+
+    return count   
+
+
+def variable_oddity(count):
+    print("There are",count,"vowel(s)")
+    if count%2 == 0:
+        print("The number of vowels in the name is even")
+    else:
+        print("The number of vowels in the name is odd")
+
+variable_oddity(count_vowels())
