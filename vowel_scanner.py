d926c2131f2d6995d22a1afaf53e09bad1a90316
Satyajit Shil <satyajitshil02@gmail.com>
2025-07-26 21:56:51 -0700

    modified

--- vowel_scanner.py
+++ vowel_scanner.py
@@ -18,4 +18,18 @@ def variable_oddity(count):
     else:
         print("The number of vowels in the name is odd")
 
-variable_oddity(count_vowels())
+#variable_oddity(count_vowels())
+def count_vowels_recursion(name, index):
+    # base condition
+    if index == len(name):
+        return 0
+    c = name[index]
+    vowels = ['a','e','i','o','u']
+    count = 0
+    if c in vowels:
+        count = 1
+    r = count_vowels_recursion(name, index + 1)
+    return count + r
+
+name = "satyajit"
+print(count_vowels_recursion(name, 0))
